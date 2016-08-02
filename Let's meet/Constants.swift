5b1cb32782e27f12199a3a8d4028e25a44851903
Do Nguyen <daniel@zincer.com>
2016-08-03 00:06:04 +0700

    Initial commit

---
+++ Let's meet/Constants.swift
@@ -0,0 +1,25 @@
+//
+//  Constants.swift
+//  Lego
+//
+//  Created by Do Nguyen on 7/28/16.
+//  Copyright © 2016 Do Nguyen. All rights reserved.
+//
+
+import UIKit
+
+// MARK: - Screen constants
+let SCREEN_SIZE = UIScreen.mainScreen().bounds.size
+let SCREEN_WIDTH = UIScreen.mainScreen().bounds.size.width
+let SCREEN_HEIGHT = UIScreen.mainScreen().bounds.size.height
+let NAV_BAR_FONT = "Avenir-Light"
+let MAIN_FONT = "Avenir-Next"
+
+// MARK: - Colors
+let MAIN_COLOR = UIColor(red: 80/255.0, green: 199/255.0, blue: 229/255.0, alpha: 1.0)
+let SECONDARY_COLOR = UIColor(red: 39/255.0, green: 179/255.0, blue: 236/255.0, alpha: 1.0)
+let BORDER_COLOR = UIColor.darkGrayColor()
+
+let NAV_COLOR = UIColor(red: 80/255.0, green: 199/255.0, blue: 229/255.0, alpha: 1.0)
+
+// MARK: - API
