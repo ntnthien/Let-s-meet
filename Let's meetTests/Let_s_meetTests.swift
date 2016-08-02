a0159f7a5c14e87dc09e1356bda1b959f0ccaba3
Do Nguyen <daniel@zincer.com>
2016-08-02 23:48:52 +0700

    Initial Commit

---
+++ Let's meetTests/Let_s_meetTests.swift
@@ -0,0 +1,36 @@
+//
+//  Let_s_meetTests.swift
+//  Let's meetTests
+//
+//  Created by Do Nguyen on 8/2/16.
+//  Copyright © 2016 Zincer. All rights reserved.
+//
+
+import XCTest
+@testable import Let_s_meet
+
+class Let_s_meetTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
