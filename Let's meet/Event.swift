8b6a186d4be1c71b2b2d942caf1e60e21675ecc1
Do Nguyen <daniel@zincer.com>
2016-08-11 17:12:31 +0700

    [ADD] Event Model

---
+++ Let's meet/Event.swift
@@ -0,0 +1,37 @@
+//
+//  Event.swift
+//  Lets meet
+//
+//  Created by Do Nguyen on 8/11/16.
+//  Copyright © 2016 Zincer. All rights reserved.
+//
+
+import Foundation
+
+struct Event {
+    var id: String
+    var location: String
+    var time: NSDate
+    var hostID: String
+    var onlineStream: Bool
+    var joinAmount: Int
+    var thumbnailURL: String
+    
+    init? (eventID: String, eventInfo: [String: AnyObject]) {
+        guard let location = eventInfo["location"] as? String,
+             time = eventInfo["time_since_1970"] as? Double,
+             hostID = eventInfo["hostID"] as? String,
+             onlineStream = eventInfo["onlineStrean"] as? Bool,
+             joinAmount = eventInfo["join_amount"] as? Int,
+             thumbnailURL = eventInfo["thumbnail_url"] as? String
+        else { return nil }
+        
+        self.id = eventID
+        self.location = location
+        self.time = NSDate.init(timeIntervalSince1970: time)
+        self.hostID = hostID
+        self.onlineStream = onlineStream
+        self.joinAmount = joinAmount
+        self.thumbnailURL = thumbnailURL
+    }
+}
