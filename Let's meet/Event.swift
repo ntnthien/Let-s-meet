131d50d1e33c907478d5dd9a4cef1534c1b6201a
Do Nguyen <daniel@zincer.com>
2016-08-12 15:44:35 +0700

    [MOD] Update Event Model

--- Let's meet/Event.swift
+++ Let's meet/Event.swift
@@ -10,31 +10,41 @@ import Foundation
 
 struct Event {
     var id: String
+    var name: String
+    var description: String
     var location: String
     var time: NSDate
     var hostID: String
     var onlineStream: Bool
     var joinAmount: Int
     var tag: [String]
+    var discussionID: String
     var thumbnailURL: String
+    
     
     init? (eventID: String, eventInfo: [String: AnyObject]) {
         guard let location = eventInfo["location"] as? String,
+             description = eventInfo["description"] as? String,
+             name = eventInfo["name"] as? String,
              time = eventInfo["time_since_1970"] as? Double,
              hostID = eventInfo["hostID"] as? String,
              onlineStream = eventInfo["onlineStrean"] as? Bool,
              joinAmount = eventInfo["join_amount"] as? Int,
+             discussionID = eventInfo["discussionID"] as? String,
              thumbnailURL = eventInfo["thumbnail_url"] as? String,
              tag = eventInfo["tag"] as? String
         else { return nil }
         
         self.id = eventID
+        self.name = name
+        self.description = description
         self.location = location
         self.time = NSDate(timeIntervalSince1970: time)
         self.hostID = hostID
         self.onlineStream = onlineStream
         self.joinAmount = joinAmount
         self.thumbnailURL = thumbnailURL
+        self.discussionID = discussionID
         self.tag = tag.componentsSeparatedByString(",")
     }
 }
