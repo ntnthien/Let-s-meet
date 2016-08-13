118f26387abcf05c15bd42fa64ed8092626560a7
Do Nguyen <daniel@zincer.com>
2016-08-13 14:38:11 +0700

    [MOD] Update Event Model
    Create Init()

--- Let's meet/Event.swift
+++ Let's meet/Event.swift
@@ -15,26 +15,13 @@ struct Event {
     var location: String
     var time: NSDate
     var hostID: String
-    var onlineStream: Bool
+    var onlineStream: String?
     var joinAmount: Int
     var tag: [String]
     var discussionID: String
-    var thumbnailURL: String
+    var thumbnailURL: String?
     
-    
-    init? (eventID: String, eventInfo: [String: AnyObject]) {
-        guard let location = eventInfo["location"] as? String,
-             description = eventInfo["description"] as? String,
-             name = eventInfo["name"] as? String,
-             time = eventInfo["time_since_1970"] as? Double,
-             hostID = eventInfo["hostID"] as? String,
-             onlineStream = eventInfo["onlineStrean"] as? Bool,
-             joinAmount = eventInfo["join_amount"] as? Int,
-             discussionID = eventInfo["discussionID"] as? String,
-             thumbnailURL = eventInfo["thumbnail_url"] as? String,
-             tag = eventInfo["tag"] as? String
-        else { return nil }
-        
+    init (id: String, name: String, description: String, location: String, time: NSDate, hostID: String, onlineStream: String?, joinAmount: Int, tag: [String], discussionID: String, thumbnailURL: String?) {
         self.id = eventID
         self.name = name
         self.description = description
@@ -44,6 +31,34 @@ struct Event {
         self.onlineStream = onlineStream
         self.joinAmount = joinAmount
         self.thumbnailURL = thumbnailURL
+        self.discussionID = discussionID
+        self.tag = tag.componentsSeparatedByString(",")
+    }
+    
+    init? (eventID: String, eventInfo: [String: AnyObject]) {
+        guard let location = eventInfo["location"] as? String,
+             description = eventInfo["description"] as? String,
+             name = eventInfo["name"] as? String,
+             time = eventInfo["time_since_1970"] as? Double,
+             hostID = eventInfo["hostID"] as? String,
+             joinAmount = eventInfo["join_amount"] as? Int,
+             discussionID = eventInfo["discussionID"] as? String,
+             tag = eventInfo["tag"] as? String
+        else { return nil }
+        
+        self.id = eventID
+        self.name = name
+        self.description = description
+        self.location = location
+        self.time = NSDate(timeIntervalSince1970: time)
+        self.hostID = hostID
+        if let onlineStream = eventInfo["onlineStream"] as? String {
+            self.onlineStream = onlineStream
+        }
+        self.joinAmount = joinAmount
+        if let thumbnailURL = eventInfo["thumbnail_url"] as? String {
+            self.thumbnailURL = thumbnailURL
+        }
         self.discussionID = discussionID
         self.tag = tag.componentsSeparatedByString(",")
     }
