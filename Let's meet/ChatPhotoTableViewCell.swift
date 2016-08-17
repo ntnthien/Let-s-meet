2ab19dea33146e060db292af51f8f1fd86c65a1c
Do Nguyen <daniel@zincer.com>
2016-08-17 23:34:23 +0700

    [ADD] changeJoinAmount API
    # Conflicts:
    #	Let's meet/Base.lproj/Main.storyboard

---
+++ Let's meet/ChatPhotoTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  ChatPhotoTableViewCell.swift
+//  Lets meet
+//
+//  Created by Nhung Huynh on 8/17/16.
+//  Copyright © 2016 Zincer. All rights reserved.
+//
+
+import UIKit
+
+class ChatPhotoTableViewCell: ChatTableViewCell {
+
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
